e2aa2450c24a3c01115ba87828cae8c1f43d421f
nguyenmanhhung16021997 <nguyenmanhhung16021997@gmail.com>
2020-09-30 22:29:54 +0700

    backend

--- src/main/java/com/codegym/crudwebservice/controller/ProductController.java
+++ src/main/java/com/codegym/crudwebservice/controller/ProductController.java
@@ -31,52 +31,24 @@ public class ProductController {
     public ResponseEntity<Product> getProduct(@PathVariable long id) {
         System.out.println("Fetching Product with id " + id);
         Product product = productService.findById(id);
-        if (product == null) {
-            System.out.println("Product with id " + id + " not found");
-            return new ResponseEntity<Product>(HttpStatus.NOT_FOUND);
-        }
         return new ResponseEntity<Product>(product, HttpStatus.OK);
     }
 
     @PostMapping("/products/")
-    public ResponseEntity<Void> createProduct(@RequestBody Product product, UriComponentsBuilder ucBuilder) {
-        System.out.println("Creating Product " + product.getName());
+    public ResponseEntity<Product> addProduct(@ModelAttribute("product") Product product) {
         productService.save(product);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(ucBuilder.path("/products/{id}").buildAndExpand(product.getId()).toUri());
-        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping("/products/{id}")
-    public ResponseEntity<Product> updateProduct(@PathVariable long id, @RequestBody Product product) {
-        System.out.println("Updating Product " + id);
-
-        Product currentProduct = productService.findById(id);
-
-        if (currentProduct == null) {
-            System.out.println("Product with id " + id + " not found");
-            return new ResponseEntity<Product>(HttpStatus.NOT_FOUND);
-        }
-
-        currentProduct.setName(product.getName());
-        currentProduct.setDescription(product.getDescription());
-        currentProduct.setId(product.getId());
-
-        productService.save(currentProduct);
-        return new ResponseEntity<Product>(currentProduct, HttpStatus.OK);
+    public ResponseEntity<Product> updateProduct(@RequestParam("product") Product product){
+        productService.save(product);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/products/{id}")
-    public ResponseEntity<Product> deleteProduct(@PathVariable long id) {
-        System.out.println("Fetching & Deleting Product with id " + id);
-
-        Product product = productService.findById(id);
-        if (product == null) {
-            System.out.println("Unable to delete. Product with id " + id + " not found");
-            return new ResponseEntity<Product>(HttpStatus.NOT_FOUND);
-        }
-
+    public ResponseEntity<Product> delete(@PathVariable Long id) {
         productService.remove(id);
-        return new ResponseEntity<Product>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/codegym/crudwebservice/model/Product.java
+++ src/main/java/com/codegym/crudwebservice/model/Product.java
@@ -20,28 +20,4 @@ public class Product {
         this.name = name;
         this.description = description;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
